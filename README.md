5562f6600325fe9c30944e97cc8450fd00ae3544
genom1997 <geno.m1997@yahoo.com>
2022-03-20 13:12:42 -0400

    added stop buttons and got video and audio working

--- README.md
+++ README.md
@@ -3,10 +3,21 @@ IoT Device - node server controlling the features of the sconce.
 
 ### installing
 
-make sure the appropriate audio player is installed and is accessible in the path, for windows the tool used is mplayer. (this could work on the raspberry pi, but omxplayer is the preferred player for the pi)
+set up raspberry os https://www.tomshardware.com/reviews/raspberry-pi-set-up-how-to,6029.html
 
-### start server locally
+install screen drivers https://www.elecrow.com/wiki/index.php?title=RC050_5_inch_HDMI_800_x_480_Capacitive_Touch_LCD_Display_for_Raspberry_Pi/_PC/_SONY_PS4
 
+make sure the appropriate audio player is installed and is accessible in the path, mplayer preferred
+make sure node is installed 
+```bash
+sudo su
+curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
+apt install nodejs
+```
+
+### start server
+
+NOTE: be sure to run as the pi user, NOT root
 run `npm start` and the server will run on localhost:8443
 
 ### design
@@ -21,3 +32,18 @@ the html page will have buttons that make requests to the other endpoints on the
 future endpoints:
 /fire - turn the screen on and play a fire animation
 /crackle - play the audio of a fire crackling
+
+### connecting to server
+
+ip address is (last checked) 192.168.1.221
+
+ssh to the server for maintenance, connect to server to activate
+
+maintenance credentials: pi:Ra$pb3rry
+
+### uploading files to the server
+
+before uploading video files, make sure they fit the proper resolution for the screen (800x480), can use this website to translate video resolutions online https://www.onlineconverter.com/resize-video
+(could resize video files with args to mplayer, but thats a future improvements)
+
+scp .\<file on local machine>.mp4 pi@192.168.1.221:/home/pi/sconce-hologram/data
--- server.js
+++ server.js
@@ -1,25 +1,56 @@
 const express = require('express');
 const { Agent } = require('http');
-var player = require('play-sound')(opts = {})
+var player = require('play-sound')(opts = {}) //a media player that will spawn a new process to play the specified media
+var videoPlayer = require('play-sound')(opts = {}) //a second media player is needed so we can play videos and audio at the same time on the host
 let app = express()
 let port = 8443
+// vars that store the current process spawned that is playing the current audio or video 
+let currentAudio;
+let currentVideo;
 
+//when a client connects, send them the control html page
 app.get('/', (req,res)=>{
     console.log('Client connected. User-agent: ' + req.headers['user-agent']);
     res.sendFile(process.cwd()+'/html/index.html');
 })
 
-app.get('/tavern', (req, res)=>{
+//when the /music endpoint on the server is hit, audio will play on the host
+app.get('/music', (req, res)=>{
+    //console.log(req.rawHeaders); TODO accept which song to play from the url params
     console.log('playing audio');
-    player.play(process.cwd()+'/data/tavern.mp3', function(err){
+    currentAudio = player.play(process.cwd()+'/data/tavern.mp3', {mplayer:['-loop','0']}, function(err){
         if (err) {
             console.log(err)
         }
     })
-    res.send('playing tavern music');
+    res.send('started playing tavern music');
 })
 
-// app.get()
+//when the /hologram endpoint on the server is hit, video will play on the host
+app.get('/hologram', (req, res)=>{
+    console.log('displaying video');
+    currentVideo = videoPlayer.play(process.cwd()+'/data/fire-low-res.mp4', {mplayer:['-loop','0']}, function(err){
+        if (err) {
+            console.log(err)
+        }
+    })
+    res.send('starting Displaying hologram');
+})
+
+//endpoints to stop the looped media from playing
+app.get('/stopaudio', (req, res)=>{
+    //console.log(req);
+    console.log('stopping audio');
+    currentAudio.kill();
+    res.send('stopped music');
+})
+
+app.get('/stopvideo', (req, res)=>{
+    //console.log(req);
+    console.log('stopping video');
+    currentVideo.kill();
+    res.send('stoped hollogram');
+})
 
 app.listen(port, () => {
     console.log(`Sconce App listening at http://localhost:${port}`)
